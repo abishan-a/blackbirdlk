b4502b602f03f65b1fbebd5dcd0011383d85b202
Amuthavasan Abishan <abishan.off@gmail.com>
2026-07-08 17:03:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -824,8 +824,9 @@ const products = [
 
    { id: 52, name: "Hotwheel F1 racing car", category: "Electronics", price: 9000, original: 8500, badge: "new", rating: 5.0, reviews: 0, freeDelivery: true, img: "product_img/Atlassian.jpg", details: { description: "Original ", material: "Standard", style: "Modern", finish: "Matte Finish", occasion: "Casual", sizes: ["Standard"], highlights: [ "🚚 FREE delivery to Vavuniya", "💵 Cash on Delivery available", "🎁 Great for gifting" ] } },
 
-   { id: 53, name: "Hotwheel F1 racing car", category: "Electronics", price: 9000, original: 8500, badge: "new", rating: 5.0, reviews: 0, freeDelivery: true, img: "product_img/kik.jpg", details: { description: "Original ", material: "Standard", style: "Modern", finish: "Matte Finish", occasion: "Casual", sizes: ["Standard"], highlights: [ "🚚 FREE delivery to Vavuniya", "💵 Cash on Delivery available", "🎁 Great for gifting" ] } }
+   { id: 53, name: "Hotwheel F1 racing car", category: "Electronics", price: 9000, original: 8500, badge: "new", rating: 5.0, reviews: 0, freeDelivery: true, img: "product_img/kik.jpg", details: { description: "Original ", material: "Standard", style: "Modern", finish: "Matte Finish", occasion: "Casual", sizes: ["Standard"], highlights: [ "🚚 FREE delivery to Vavuniya", "💵 Cash on Delivery available", "🎁 Great for gifting" ] } },
 
+   { id: 54, name: "Hotwheel F1 racing car", category: "Electronics", price: 9000, original: 8500, badge: "new", rating: 5.0, reviews: 0, freeDelivery: true, img: "product_img/benz.jpg", details: { description: "Original ", material: "Standard", style: "Modern", finish: "Matte Finish", occasion: "Casual", sizes: ["Standard"], highlights: [ "🚚 FREE delivery to Vavuniya", "💵 Cash on Delivery available", "🎁 Great for gifting" ] } }q
     
 ];
 
